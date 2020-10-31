f1ff7fa33f8131870402b50fcc6f273dc1566526
OriolJT <oriol.julia@e-campus.uab.cat>
2020-10-31 16:52:52 +0100

    test Initialize0Bombs()

--- src/test/java/core/TestBoard.java
+++ src/test/java/core/TestBoard.java
@@ -43,14 +43,28 @@ public class TestBoard {
 		assertEquals(10, bombs);
 	}
 	
+	public void testInitialized0Bombes() { //comprovació valor limit 0 bombes.
+		Board b = new BoardR(5, 6, 0);
+		
+		b.openSquare(5, 3);
+		Square[][] R = b.getBoard();
+		
+		int bombs = 0;
+		for(int i = 0; i < b.getRows(); i++) {
+			for (int j = 0; j < b.getCols(); i++) {
+				bombs += R[i][j].getIsBomb()? 0 : 1;
+			}
+		}
+		
+		assertEquals(0, bombs);
+	}
+	
+	
 	/*
 	public void testOpenSquare() {
 		Board b = new BoardR(5, 6, 10);
 		b.openSquare(5, 3);
 		b.getInitialized();
-		
-		
-		
-	}*/
+		}*/
 	
 }
