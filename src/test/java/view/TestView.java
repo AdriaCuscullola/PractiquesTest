ddc664f2573399c2760cf4975265f64533ff2f2f
MarcSalaManzano <marc.salam@e-campus.uab.cat>
2020-11-05 10:05:10 +0100

    Test for views button creation

--- src/test/java/view/TestView.java
+++ src/test/java/view/TestView.java
@@ -15,6 +15,20 @@ public class TestView {
 		View view = new RView(12, 10, mock);
 		assertEquals(12, view.getRows());
 		assertEquals(10, view.getCols());
+		
+		JButtonExtend[][] buttons = view.getButtons();
+		for(int i = 0; i < 12; i++) {
+			for(int j = 0; j < 10; j++) {
+				assertEquals(true, buttons[i][j].isEnabled());
+				assertEquals("", buttons[i][j].getText());
+				assertEquals(i, buttons[i][j].getRow());
+				assertEquals(j, buttons[i][j].getCol());
+			}
+		}
+		assertEquals(12, buttons.length);
+		assertEquals(10, buttons[0].length);
 	}
+	
+	
 
 }
