876b17b233aa6f30b6a09b071132438a158b17bd
MarcSalaManzano <marc.salam@e-campus.uab.cat>
2020-11-10 17:37:55 +0100

    Test travis

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,8 @@ import view.RView;
 
 public class Main {
 	public static void main(String[] args) {
-		Game game = new Game(12, 10, 15);
-		RView view = new RView(12, 10, game);
+		Game game = new Game(30, 30, 99);
+		RView view = new RView(30, 30, game);
 		game.setView(view);
 	}
 }
--- src/main/java/controller/GameInterface.java
+++ src/main/java/controller/GameInterface.java
@@ -5,34 +5,15 @@ import java.awt.event.MouseListener;
 
 public interface GameInterface extends MouseListener{
 
-	@Override
-	default void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+	
+	void mouseClicked(MouseEvent arg0);
 
-	@Override
-	default void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+	void mouseEntered(MouseEvent arg0);
 
-	@Override
-	default void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+	void mouseExited(MouseEvent arg0);
 
-	@Override
-	default void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+	void mousePressed(MouseEvent arg0);
 
-	@Override
-	default void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+	void mouseReleased(MouseEvent arg0);
 
 }
